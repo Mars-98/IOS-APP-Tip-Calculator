cca13dd042b9c931094110354a17975d0be10dee
mary <adams222@uwm.edu>
2020-07-08 14:20:16 -0500

    Initial Commit

---
+++ TipCalculator2Tests/TipCalculator2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TipCalculator2Tests.swift
+//  TipCalculator2Tests
+//
+//  Created by Mariam Adams on 7/8/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import XCTest
+@testable import TipCalculator2
+
+class TipCalculator2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
