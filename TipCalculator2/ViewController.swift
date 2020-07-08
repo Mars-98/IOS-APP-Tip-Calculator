0f3e335d9c087ccef50a744265ecd3b9d8093458
mary <adams222@uwm.edu>
2020-07-08 17:48:15 -0500

    Finished after 5 hourssss

--- TipCalculator2/ViewController.swift
+++ TipCalculator2/ViewController.swift
@@ -10,11 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var eachPayLabel: UILabel!
+    @IBOutlet weak var eachPersonAmountLabel: UILabel!
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var peopleSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateBillAmount() {
+        let bill = Double(amountTextField.text!) ?? 0
+        
+        //Calculate the tip and total
+        let tipPercentages = Double(Double(tipPercentageSlider.value) / 100)
+        
+        //Updating the total amount
+        let tip = bill * tipPercentages
+        let totalWithoutSplit = bill + tip
+        totalAmountLabel.text = String(format: "$%0.2f", totalWithoutSplit)
+        
+        // Updating the people split
+        let people = Int(peopleSlider.value)
+        let eachPerson = totalWithoutSplit / Double(people)
+        eachPersonAmountLabel.text = String(format: "$%0.02f", eachPerson)
+        
+        //calculateBill()
+        
+    }
+    
+    @IBAction func tipSliderValue(_ sender: Any) {
+        tipPercentageLabel.text = String(format:"Tip: %d%%", Int(tipPercentageSlider.value))
+        calculateBillAmount()
+    }
+    
+    
+    @IBAction func numberOfPeople(_ sender: Any) {
+        splitLabel.text = String(format:"Split: %d", Int(peopleSlider.value))
+        calculateBillAmount()
+    }
+    @IBAction func amount(_ sender: Any) {
+        calculateBillAmount()
+    }
+    
+    
 }
 
